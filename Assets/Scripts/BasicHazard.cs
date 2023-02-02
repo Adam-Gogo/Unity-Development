c4bb757e92aa5c359ced8f93b76170cde49f0c79
RDT022 <adamromanowski12@gmail.com>
2023-02-01 20:46:02 -0500

    moving enemy

--- Assets/Scripts/BasicHazard.cs
+++ Assets/Scripts/BasicHazard.cs
@@ -7,6 +7,23 @@ public class BasicHazard : MonoBehaviour
     [SerializeField]
     playerMovement player;
 
+    public float leftX;
+    public float rightX;
+    public float speed;
+
+    bool facingRight = true;
+
+    public void Update()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+        Vector3 rot = new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + 180, transform.rotation.eulerAngles.z);
+        if ((transform.position.x <= leftX && facingRight == false) || (transform.position.x >= rightX && facingRight == true))
+        {
+            transform.rotation = Quaternion.Euler(rot);
+            facingRight = !facingRight;
+        }
+    }
+
     public void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.GetComponent<playerMovement>() == player)
