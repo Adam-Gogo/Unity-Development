49077c5f8531a7c6e9752d8f2d930817d99c6041
RDT022 <adamromanowski12@gmail.com>
2023-03-30 19:50:07 -0400

    fixed enemies + another level

--- Assets/Scripts/BasicHazard.cs
+++ Assets/Scripts/BasicHazard.cs
@@ -7,20 +7,37 @@ public class BasicHazard : MonoBehaviour
     [SerializeField]
     playerMovement player;
 
-    public float leftX;
-    public float rightX;
+    public float pos1;
+    public float pos2;
     public float speed;
 
-    bool facingRight = true;
+    bool movingRight = true;
+
+    [SerializeField]
+    bool horizontal;
 
     public void Update()
     {
-        transform.Translate(Vector3.right * speed * Time.deltaTime);
-        Vector3 rot = new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + 180, transform.rotation.eulerAngles.z);
-        if ((transform.position.x <= leftX && facingRight == false) || (transform.position.x >= rightX && facingRight == true))
+        if (horizontal)
         {
-            transform.rotation = Quaternion.Euler(rot);
-            facingRight = !facingRight;
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            Vector3 rot = new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + 180, transform.rotation.eulerAngles.z);
+            if ((transform.position.x <= pos1 && movingRight == false) || (transform.position.x >= pos2 && movingRight == true))
+            {
+                transform.rotation = Quaternion.Euler(rot);
+                movingRight = !movingRight;
+            }
+        }
+        else
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            Vector3 rot = new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + 180, transform.rotation.eulerAngles.z);
+            if ((transform.position.z <= pos1 && movingRight == false) || (transform.position.z >= pos2 && movingRight == true))
+            {
+                transform.rotation = Quaternion.Euler(rot);
+                movingRight = !movingRight;
+            }
+
         }
     }
 
